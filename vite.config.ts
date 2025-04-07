88372c2b5ef7e5302b1ef9f3c52ac65fe9465a10
Mohan-codehub <105050325+Mohan-codehub@users.noreply.github.com>
2025-04-08 01:02:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/spottournament/', // 👈 Set this to your GitHub repo name
+  base: '/spottournament/', // 👈 Required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
