5598f0cff823f2fbd15de754595877526fd7ab05
Mohan Kumar S <mohan_s@Mohans-MacBook-Air.local>
2025-04-14 12:36:46 +0530

    remoced vit.sbg

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/spottournament/', // 👈 Required for GitHub Pages
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/spottournament/', // Replace 'spottournament' with your repository name
 });
